a5fad27a18fd6ad0e6ff4d7b96e9de69550b0751
teinei <teinei@yahoo.com>
2018-06-16 10:56:42 +0800

    report1, array added

--- Rise3/crud-surveys/report1-average.php
+++ Rise3/crud-surveys/report1-average.php
@@ -49,6 +49,15 @@ if(isset($_POST['class_number'])){
 
 	$row_average=0;
 	$count=0;
+
+	//
+	$student_names=array();
+	$q1=array();
+	$q2=array();
+	$q3=array();
+	$q4=array();
+	$q5=array();
+
 	while ( $row = $stmt->fetch(PDO::FETCH_ASSOC)) {
 		$row_array=array();
 		//echo '$count: '."$count<br>";
@@ -60,9 +69,21 @@ if(isset($_POST['class_number'])){
 			echo "<td>$value</td>";
 			$row_array[]=$value;
 		} 
+		$student_names[]=$row_array[1];
+		//echo "<br>";
+		//var_dump($student_names);
+		//echo "<br>";
+
+		$q1[]=$row_array[2];
+		$q2[]=$row_array[3];
+		$q3[]=$row_array[4];
+		$q4[]=$row_array[5];
+		$q5[]=$row_array[6];
+		//
 		//echo"<br>";
 		echo "</tr>";
 		
+		//print row array
 		//echo "<br>";
 		//var_dump($row_array);
 		//echo "<br>";
@@ -70,7 +91,22 @@ if(isset($_POST['class_number'])){
 		//echo '$class_total: '."$class_total<br>";
 		$count=$count+1;
 		//echo  '$count: '."$count<br>";
-	}
+	}//end of while loop
+	echo "<br>";
+	var_dump($student_names);
+	echo "<br>";
+	var_dump($q1);
+	echo "<br>";
+	var_dump($q2);
+	echo "<br>";
+	var_dump($q3);
+	echo "<br>";
+	var_dump($q4);
+	echo "<br>";
+	var_dump($q5);
+	echo "<br>";
+	//
+	//
 	$class_average=$class_total/$count*10;
 ?>
 <tr>
